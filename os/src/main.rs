c021083b241d630e4c3318915f9932a087335e4e
chi030303 <1726634055@qq.com>
2024-01-03 22:28:10 +0800

    21301151

--- os/src/main.rs
+++ os/src/main.rs
@@ -20,8 +20,8 @@ mod loader;
 mod config;
 mod task;
 mod timer;
-mod mm;
 mod sync;
+mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -46,9 +46,12 @@ pub fn rust_main() -> ! {
     mm::init();
     println!("[kernel] back to world!");
     mm::remap_test();
+    task::add_initproc();
+    println!("after initproc!");
     trap::init();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
-    task::run_first_task();
+    loader::list_apps();
+    task::run_tasks();
     panic!("Unreachable in rust_main!");
 }
